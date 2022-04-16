1d4bb091bbf549635857ba19a1dc4509787e5c44
KirillVN <kirill137777@gmail.com>
2022-04-16 14:33:20 +0300

    commit

--- app.js
+++ app.js
@@ -1,41 +1,36 @@
 const icon = document.querySelector(".icon-menu");
 const appearList = document.getElementById("menu-appear");
 const exitBtn = document.querySelector(".exit-btn");
-
-// icon.addEventListener('click', function(){
-//     appearList.classList.add('btn-appear');
-
-// })
-
-// exitBtn.addEventListener('click', function (){
-//     appearList.classList.remove('btn-appear');
-
-// });
+const header = document.querySelector('header');
 
 icon.addEventListener("click", () => {
   appearList.classList.toggle("btn-appear");
 });
-
 exitBtn.addEventListener("click", () => {
     appearList.classList.remove("btn-appear");
 });
-
-
-const header = document.querySelector('header');
-
 window.addEventListener('scroll', () => {
 const headerHeight = header.getBoundingClientRect().height;
 const scrollHeight = window.pageYOffset;
-
 if(scrollHeight > headerHeight) {
   header.classList.add('fixed-position');
 } else {
   header.classList.remove('fixed-position');
 }
-
 })
 
-
-
+// TYPE WRITER EFFECT
+// const text = document.querySelector('.main-title').innerText;
+let text = 'My name is Kirill & this is my Blog';
+const container = document.querySelector('.main-title')
+let i = 0;
+function typeWriter() {
+  if(i < text.length) {
+    container.innerHTML += text.charAt(i)
+    i++;
+    setTimeout(typeWriter, 50)
+  }
+}
+window.addEventListener('load', typeWriter)
 
   
